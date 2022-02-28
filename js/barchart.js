b16e97fa597d5498217482010fc885b79ef1bd82
abbycarr <carr.ab@husky.neu.edu>
2022-02-28 12:46:02 -0500

    RESET files. Major issues

--- js/barchart.js
+++ js/barchart.js
@@ -1,12 +1,13 @@
-// COMMENTING DONE IN CLASS 02/24
-
 /*
 
-In-class activity 08 starter code
-Prof. Mosca 
-Modified: 12/08/21 
+ In-class activity 08 starter code
+ Prof. Mosca 
+ Modified: 12/08/21 
 
-*/
+ */
+
+// Build your bar charts in this file 
+
 
 // Set dimensions and margins for plots 
 const width = 900;
@@ -15,20 +16,12 @@ const margin = { left: 50, right: 50, bottom: 50, top: 50 };
 const yTooltipOffset = 15;
 
 
-// Make an element in d3 
+// TODO: What does this code do? 
 const svg1 = d3
-  // select the HTML element with the id hard-coded-bar
   .select("#hard-coded-bar")
-  // add an svg to the selected element
   .append("svg")
-  // set the SVG to width constant minus the left and the 
-  //  right of the margin consannt
   .attr("width", width - margin.left - margin.right)
-  // set the SVG to height constant minus the top and the
-  //  bottom of the margin consannt
   .attr("height", height - margin.top - margin.bottom)
-  // set the SVG viewBox to 0, 0, the width constant, and
-  //  the height constant
   .attr("viewBox", [0, 0, width, height]);
 
 // Hardcoded barchart data
@@ -42,31 +35,13 @@ const data1 = [
   { name: 'G', score: 18 }
 ];
 
-
-// // Make an element in d3 
-// const svg2 = d3
-//   // select the HTML element with the id hard-coded-bar
-//   .select("#csv-bar")
-//   // add an svg to the selected element
-//   .append("svg")
-//   // set the SVG to width constant minus the left and the 
-//   //  right of the margin consannt
-//   .attr("width", width - margin.left - margin.right)
-//   // set the SVG to height constant minus the top and the
-//   //  bottom of the margin consannt
-//   .attr("height", height - margin.top - margin.bottom)
-//   // set the SVG viewBox to 0, 0, the width constant, and
-//   //  the height constant
-//   .attr("viewBox", [0, 0, width, height]);
-
 /*
 
   Axes
 
 */
 
-// use D3 to get the max "score" from the dictionaries in the
-//  constant data1
+// TODO: What does this code do? 
 let maxY1 = d3.max(data1, function (d) { return d.score; });
 
 // TODO: What does each line of this code do?   
@@ -80,9 +55,6 @@ let xScale1 = d3.scaleBand()
   .range([margin.left, width - margin.right])
   .padding(0.1);
 
-
-// let yScale2 = 
-
 // TODO: What does each line of this code do?  
 svg1.append("g")
   .attr("transform", `translate(${margin.left}, 0)`)
@@ -95,19 +67,6 @@ svg1.append("g")
   .call(d3.axisBottom(xScale1)
     .tickFormat(i => data1[i].name))
   .attr("font-size", '20px');
-
-// // TODO: What does each line of this code do?  
-// svg2.append("g")
-//   .attr("transform", `translate(${margin.left}, 0)`)
-//   .call(d3.axisLeft(yScale1))
-//   .attr("font-size", '20px');
-
-// // TODO: What does each line of this code do? 
-// svg2.append("g")
-//   .attr("transform", `translate(0,${height - margin.bottom})`)
-//   .call(d3.axisBottom(xScale1)
-//     .tickFormat(i => data1[i].name))
-//   .attr("font-size", '20px'); 
 
 /* 
 
@@ -158,4 +117,3 @@ svg1.selectAll(".bar")
   .on("mouseover", mouseover1)
   .on("mousemove", mousemove1)
   .on("mouseleave", mouseleave1);
-
